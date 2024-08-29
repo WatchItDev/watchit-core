802036ed906b00795c158c6fa1cf29d7d0b24870
Geolffrey Mena <gmjun2000@gmail.com>
2024-08-29 12:15:26 -0600

    refactor: interfaces segregations

--- test/internals/DistributorFinancial.t.sol
+++ test/internals/DistributorFinancial.t.sol
@@ -3,9 +3,15 @@ pragma solidity 0.8.24;
 import "forge-std/Test.sol";
 import "contracts/base/upgradeable/Distributor.sol";
 
+
 contract DistributorFinancialTest is Test, Disitributor {
 
-    function test_NegotiationPlusConstantFees() {}
+    function test_NegotiationPlusConstantFeesIncrementalDemand() {
+        uint256 demand = 1000; // under 1000 content custodial..
+        console.log(_getAdjustedFloor(1e8, 1000));
+    }
+
+
     function test_NegotiationPlusVariantFees() {}
     function test_NegotiationPlusVariantPlusFlattentFactorFees() {}
     function test_NegotiationAutoAdjustedFactorFees() {}
