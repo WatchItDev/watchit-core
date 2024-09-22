6820d05c4525aace493e8b2842aa867c0193d4ec
Geolffrey Mena <gmjun2000@gmail.com>
2024-09-22 14:11:31 -0600

    refactor: concrent interfaces

--- contracts/interfaces/IRightsManager.sol
+++ contracts/interfaces/IRightsManager.sol
@@ -7,7 +7,6 @@ import "./IDisburser.sol";
 import "./IContentVault.sol";
 import "./IRightsCustodial.sol";
 import "./IBalanceManager.sol";
-import "./IRightsAgreementBroker.sol";
 import "./IRightsPolicyAuditor.sol";
 import "./IRightsPolicyController.sol";
 import "./IRightsCustodialGranter.sol";
@@ -19,7 +18,6 @@ interface IRightsManager is
     IFeesManager,
     IBalanceManager,
     IRightsCustodial,
-    IRightsAgreementBroker,
     IRightsAccessController,
     IRightsCustodialGranter,
     IRightsPolicyController,
@@ -54,6 +52,24 @@ interface IRightsManager is
         address policyAddress,
         bytes calldata data
     ) external payable;
+
+        /// @notice Retrieves a agreement associated with the given proof.
+    /// @param proof The unique identifier of the agreement to retrieve.
+    /// @return agreement The agreement object associated with the provided proof.
+    function getAgreement(bytes32 proof) external view returns (T.Agreement memory);
+
+    /// @notice Creates a new agreement between the account and the content holder, returning a unique agreementidentifier.
+    /// @param total The total amount involved in the agreement.
+    /// @param currency The address of the ERC20 token (or native currency) being used in the agreement.
+    /// @param holder The address of the content holder whose content is being accessed.
+    /// @param account The address of the account proposing the agreement.
+    /// @return bytes32 A unique identifier (agreementProof) representing the created agreement.
+    function createAgreement(
+        uint256 total,
+        address currency,
+        address holder,
+        address account
+    ) external returns (bytes32);
 
     /// @notice Executes the creation of an agreement and immediately registers the policy in a single transaction.
     /// @param total The total amount involved in the agreement.
